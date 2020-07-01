0fb537e28f908fa88eabf7d58e8bdf9d27653d8e
Vuong Hai Thanh <thanhvh@MacBook-Pro-cua-Vuong.local>
2020-07-01 14:51:09 +0700

    Nop bai

---
+++ STRINGS.ts
@@ -0,0 +1,24 @@
+import LocalizedStrings from 'react-native-localization';
+
+const strings: any = new LocalizedStrings({
+  en: {
+    TITLE: 'Voice Calculator',
+    PLAY: 'Play',
+    PAUSE: 'Pause',
+    STOP: 'Stop',
+    RECORD: 'Record',
+    CALCULATOR: 'Calculator'
+
+  },
+  ko: {
+    TITLE: '오디오 녹음 / 플레이어',
+    PLAY: '재생',
+    PAUSE: '일시정지',
+    STOP: '정지',
+    RECORD: '녹화',
+  },
+});
+
+export const getString = (str: string) => {
+  return strings[str];
+};
